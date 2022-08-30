01c834a5e19e0deba1808af00b0e32e9c9017915
Shreyash <superbshreyash@gmail.com>
2022-08-30 15:36:05 +0530

    Updates

---
+++ precache-manifest.43f015c1535d7275eac220fe1b4f3554.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a8f3d93535275554af4",
+    "url": "/Text-Analyzer/static/css/main.03e64cd5.chunk.css"
+  },
+  {
+    "revision": "1a8f3d93535275554af4",
+    "url": "/Text-Analyzer/static/js/main.1582d855.chunk.js"
+  },
+  {
+    "revision": "f228b362c905362c0b3a",
+    "url": "/Text-Analyzer/static/js/runtime~main.b3439972.js"
+  },
+  {
+    "revision": "50e766cde03e48874228",
+    "url": "/Text-Analyzer/static/js/2.aa014faa.chunk.js"
+  },
+  {
+    "revision": "2ded26446bc94b81347a",
+    "url": "/Text-Analyzer/static/js/3.398cb210.chunk.js"
+  },
+  {
+    "revision": "f35744368fa31a23649e5ede51b7daf6",
+    "url": "/Text-Analyzer/index.html"
+  }
+];
